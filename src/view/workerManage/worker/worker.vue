470e72c41a301374e16d92c7f4ef95c2b341d3dc
王雪 <2713193934@qq.com>
2022-07-06 09:50:17 +0800

    手机号正则判断

--- src/view/workerManage/worker/worker.vue
+++ src/view/workerManage/worker/worker.vue
@@ -595,7 +595,7 @@ const rules = ref({
   ],
   gzrysjh: [
     { required: true, message: '请输入手机号', trigger: 'blur' },
-    { pattern: /^1(3|4|5|7|8|9)\d{9}$/,
+    { pattern: /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/,
       message: '手机号不合法',
       trigger: 'blur' },
     // { type: 'number', message: 'must be a number' },
